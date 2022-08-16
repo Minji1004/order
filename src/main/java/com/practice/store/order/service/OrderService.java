86bac3b0caf296a3b2751c99411d8db04f7557b1
이민지 <energizermj104@gmail.com>
2022-08-16 13:41:43 +0900

    랜덤 문자열 추가

--- src/main/java/com/practice/store/order/service/OrderService.java
+++ src/main/java/com/practice/store/order/service/OrderService.java
@@ -26,9 +26,23 @@ public class OrderService {
 
         UserEntity userEntity = userService.getUserEntity(email, userId);
         // orderNo 중복 체크 해야함.
-        String orderNo = StringUtil.genRandomString(12);
+        String orderNo = generatedOrderNo();
         OrderEntity orderEntity = new OrderEntity(orderNo, orderRequest.getItemName(), LocalDateTime.now(), userEntity);
 
         orderIRepository.save(orderEntity);
+    }
+
+    private String generatedOrderNo(){
+        String orderNo = StringUtil.genRandomString(12);
+        while(isDuplicatedOrderNo(orderNo)){
+            orderNo = StringUtil.genRandomString(12);
+        }
+
+        return orderNo;
+    }
+
+    private boolean isDuplicatedOrderNo(String orderNo){
+        OrderEntity orderEntity = orderIRepository.findById(orderNo).orElse(null);
+        return orderEntity != null;
     }
 }
